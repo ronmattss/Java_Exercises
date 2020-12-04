53760cc2476e0de27165730a6406b9aacf46b7d9
ronmattss <31378489+ronmattss@users.noreply.github.com>
2020-12-04 20:34:40 +0800

    Yes

---
+++ parserbasedcalculator/src/com/shadedgames/Main.java
@@ -0,0 +1,28 @@
+package com.shadedgames;
+
+import java.io.*;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        // write your code here
+        String expr;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Parser p = new Parser();
+        System.out.println("Enter an empty expression to stop.");
+        for (; ; ) {
+            System.out.print("Enter expression: ");
+            expr = br.readLine();
+            if (expr.equals("")) break;
+            try {
+                System.out.println("Result: " + p.evaluate(expr));
+                System.out.println();
+            } catch (ParserException exc) {
+                System.out.println(exc);
+            }
+            
+        }
+    }
+}
+
